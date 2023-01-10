1f74ed9ab3fb8ffdcad25c22b2d4549ab543c3a8
kcy013100 <kcy013100@naver.com>
2023-01-10 20:36:52 +0900

    2023/01/10 home

---
+++ Ch07/src/main/java/kr/co/ch07/vo/User5VO.java
@@ -0,0 +1,21 @@
+package kr.co.ch07.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User5VO {
+	private String uid;
+	private String name;
+	private String birth;
+	private int gender;
+	private int age;
+	private String addr;
+	private String hp;
+
+}
---
+++ Ch07/src/main/java/kr/co/ch07/vo/User6VO.java
@@ -0,0 +1,20 @@
+package kr.co.ch07.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User6VO {
+	private String uid;
+	private String name;
+	private String birth;
+	private int age;
+	private String address;
+	private String hp;
+
+}
