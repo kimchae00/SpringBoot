16536adefacfc0cb6d9a957b823f13d84c53c2aa
kcy013100 <kcy013100@naver.com>
2023-03-31 03:24:06 +0900

    dd

--- HelloPet/src/main/java/kr/co/hellopet/mail/MailSendService.java
+++ HelloPet/src/main/java/kr/co/hellopet/mail/MailSendService.java
@@ -31,7 +31,7 @@ public class MailSendService {
 		makeRandomNumber();
 		String setFrom = "leemh4390@gmail.com";
 		String toMail = email;
-		String title = "이건 테스트입니다.";
+		String title = "HelloPet 이메일 인증번호.";
 		String content = 
 						"HelloPet TEST." + 	//html 형식으로 작성 ! 
 		                "<br><br>" + 
